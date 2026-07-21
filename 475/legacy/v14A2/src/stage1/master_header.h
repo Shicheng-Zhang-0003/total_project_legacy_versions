1f1680656e6eab4ef2df13e850108c7a3f708291
Shicheng_Zhang-0003 <shicheng_zhang@outlook.com>
2026-07-21 15:28:25 -0600

    21/07/26: v14A3 started with Antigravity CLI, A2 moved to legacy compilation for now

---
+++ 475/legacy/v14A2/src/stage1/master_header.h
@@ -0,0 +1,15 @@
+#ifndef master_h
+#define master_h
+
+#define MPE_MAX_BODIES 16384
+#define MPE_MAX_JOINTS 1024
+#define MPE_MAX_BROADPHASE_PAIRS 65536
+
+//master header file for calling all library files
+#include "math/math3D.h" //Math
+#include "math/math4_special.h" //Math
+#include "math_phys_buffer/buffer.h" //Buffer
+#include "physics/collision_mechanics/collision_mechanics.h" //Collision Mechanics
+#include "physics/forces/defines_majority_forces/define_forces.h" //Physics (Forces and Energy)
+
+#endif
